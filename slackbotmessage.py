4720f752fd671d25dfe08c5828df41c7d07c0544
Crazy0416 <minmin0416@naver.com>
2016-08-26 21:45:49 +0900

    Update slackbotmessage.py

--- slackbotmessage.py
+++ slackbotmessage.py
@@ -1,3 +1,3 @@
 import requests
 
-r = requests.post(requests.post('https://hooks.slack.com/services/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', json={"attachments": [ {"title": "Go to Facebook","title_link": "https://www.facebook.com"}]})
+r = requests.post('https://hooks.slack.com/services/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', json={"attachments": [ {"title": "Go to Facebook","title_link": "https://www.facebook.com"}]})
